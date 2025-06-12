fc50ec9e3703f6ee8789efb251fe7308d1e36b02
Calvin Pangilinan <calvinpangilinan13@gmail.com>
2025-06-12 12:22:33 +0800

    try2

--- system/core/Controller.php
+++ system/core/Controller.php
@@ -1,23 +1,55 @@
 <?php
-defined('BASEPATH') or exit('No direct script access allowed');
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2014 - 2019, British Columbia Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package	CodeIgniter
+ * @author	EllisLab Dev Team
+ * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (https://ellislab.com/)
+ * @copyright	Copyright (c) 2014 - 2019, British Columbia Institute of Technology (https://bcit.ca/)
+ * @license	https://opensource.org/licenses/MIT	MIT License
+ * @link	https://codeigniter.com
+ * @since	Version 1.0.0
+ * @filesource
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-class CI_Controller
-{
-
-	// Explicit property declarations to avoid deprecated warnings
-	public $benchmark;
-	public $hooks;
-	public $config;
-	public $log;
-	public $utf8;
-	public $uri;
-	public $router;
-	public $output;
-	public $security;
-	public $input;
-	public $lang;
-	public $load;
-	public $db;
+/**
+ * Application Controller Class
+ *
+ * This class object is the super class that every library in
+ * CodeIgniter will be assigned to.
+ *
+ * @package		CodeIgniter
+ * @subpackage	Libraries
+ * @category	Libraries
+ * @author		EllisLab Dev Team
+ * @link		https://codeigniter.com/user_guide/general/controllers.html
+ */
+class CI_Controller {
 
 	/**
 	 * Reference to the CI singleton
@@ -25,6 +57,13 @@ class CI_Controller
 	 * @var	object
 	 */
 	private static $instance;
+
+	/**
+	 * CI_Loader
+	 *
+	 * @var	CI_Loader
+	 */
+	public $load;
 
 	/**
 	 * Class constructor
@@ -35,7 +74,11 @@ class CI_Controller
 	{
 		self::$instance =& $this;
 
-		foreach (is_loaded() as $var => $class) {
+		// Assign all the class objects that were instantiated by the
+		// bootstrap file (CodeIgniter.php) to local class variables
+		// so that CI can run as one big super object.
+		foreach (is_loaded() as $var => $class)
+		{
 			$this->$var =& load_class($class);
 		}
 
@@ -43,6 +86,8 @@ class CI_Controller
 		$this->load->initialize();
 		log_message('info', 'Controller Class Initialized');
 	}
+
+	// --------------------------------------------------------------------
 
 	/**
 	 * Get the CI singleton
@@ -54,4 +99,5 @@ class CI_Controller
 	{
 		return self::$instance;
 	}
+
 }
