ed27cfb72364482ec7a65691fdc66b0ee968e456
Calvin Pangilinan <calvinpangilinan13@gmail.com>
2025-06-12 12:57:43 +0800

    try 4

--- system/core/Loader.php
+++ system/core/Loader.php
@@ -4,54 +4,21 @@
  *
  * An open source application development framework for PHP
  *
- * This content is released under the MIT License (MIT)
- *
- * Copyright (c) 2014 - 2019, British Columbia Institute of Technology
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
  * @package	CodeIgniter
  * @author	EllisLab Dev Team
- * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (https://ellislab.com/)
- * @copyright	Copyright (c) 2014 - 2019, British Columbia Institute of Technology (https://bcit.ca/)
  * @license	https://opensource.org/licenses/MIT	MIT License
  * @link	https://codeigniter.com
  * @since	Version 1.0.0
- * @filesource
  */
+
 defined('BASEPATH') or exit('No direct script access allowed');
 
-/**
- * Loader Class
- *
- * Loads framework components.
- *
- * @package		CodeIgniter
- * @subpackage	Libraries
- * @category	Loader
- * @author		EllisLab Dev Team
- * @link		https://codeigniter.com/user_guide/libraries/loader.html
- */
+// Add this attribute to suppress dynamic property deprecation in PHP 8.2+
+#[\AllowDynamicProperties]
 class CI_Loader
 {
-
-	// All these are set automatically. Don't mess with them.
+	// Add this property to prevent dynamic assignment warning
+	public $load;
 
 	/**
 	 * Nesting level of the output buffering mechanism
@@ -60,91 +27,27 @@ class CI_Loader
 	 */
 	protected $_ci_ob_level;
 
-	// 🔧 Add this line to prevent the deprecation warning in PHP 8.2+
-	public $load;
-
-	/**
-	 * List of paths to load views from
-	 *
-	 * @var	array
-	 */
 	protected $_ci_view_paths = array(VIEWPATH => TRUE);
-
-	/**
-	 * List of paths to load libraries from
-	 *
-	 * @var	array
-	 */
 	protected $_ci_library_paths = array(APPPATH, BASEPATH);
-
-	/**
-	 * List of paths to load models from
-	 *
-	 * @var	array
-	 */
 	protected $_ci_model_paths = array(APPPATH);
-
-	/**
-	 * List of paths to load helpers from
-	 *
-	 * @var	array
-	 */
 	protected $_ci_helper_paths = array(APPPATH, BASEPATH);
-
-	/**
-	 * List of cached variables
-	 *
-	 * @var	array
-	 */
 	protected $_ci_cached_vars = array();
-
-	/**
-	 * List of loaded classes
-	 *
-	 * @var	array
-	 */
 	protected $_ci_classes = array();
-
-	/**
-	 * List of loaded models
-	 *
-	 * @var	array
-	 */
 	protected $_ci_models = array();
-
-	/**
-	 * List of loaded helpers
-	 *
-	 * @var	array
-	 */
 	protected $_ci_helpers = array();
 
-	/**
-	 * List of class name mappings
-	 *
-	 * @var	array
-	 */
 	protected $_ci_varmap = array(
 		'unit_test' => 'unit',
 		'user_agent' => 'agent'
 	);
 
-	// --------------------------------------------------------------------
-
-	/**
-	 * Class constructor
-	 *
-	 * Sets component load paths, gets the initial output buffering level.
-	 *
-	 * @return	void
-	 */
 	public function __construct()
 	{
 		$this->_ci_ob_level = ob_get_level();
 		$this->_ci_classes =& is_loaded();
-
 		log_message('info', 'Loader Class Initialized');
 	}
+
 
 	// --------------------------------------------------------------------
 
