d93f231b6a52fad78c6ea09b2d22da1a587a2375
Calvin Pangilinan <calvinpangilinan13@gmail.com>
2025-06-12 14:19:58 +0800

    lg3

--- application/controllers/admin/Dashboard.php
+++ application/controllers/admin/Dashboard.php
@@ -1,22 +1,25 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Dashboard extends CI_Controller {
-	function __construct(){
+class Dashboard extends CI_Controller
+{
+	function __construct()
+	{
 		parent::__construct();
 		$this->load->model('admin/Dashboard_Model');
-		if(!$this->session->userdata('uid'))
-		redirect('admin/login');
-		}
-	
+		// if(!$this->session->userdata('uid'))
+		// redirect('admin/login');
+	}
+
 	public function index()
-	{   $this->load->model('admin/Dashboard_Model','Dashboard');
+	{
+		$this->load->model('admin/Dashboard_Model', 'Dashboard');
 		$data['totalnew'] = $this->Dashboard->totalnewcount();
 		$data['totalcategory'] = $this->Dashboard->totalcategorycount();
 		$data['totalsubcategory'] = $this->Dashboard->totalsubcategorycount();
 		$data['totalcomment'] = $this->Dashboard->totalcommentcount();
 		$data['totaladmin'] = $this->Dashboard->totaladmincount();
-		$this->load->view('admin/dashboard',$data);
+		$this->load->view('admin/dashboard', $data);
 
 	}
 }
