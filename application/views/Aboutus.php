ed53b0ac3eb20b2b7a16522182b1a2f84a8630ac
Calvin Pangilinan <calvinpangilinan13@gmail.com>
2025-06-13 11:22:52 +0800

    tog1

--- application/views/Aboutus.php
+++ application/views/Aboutus.php
@@ -218,6 +218,7 @@
     </div>
 
     <?php include APPPATH . 'views/includes/footer.php'; ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
