42e1c0d49e53f706419548956a83d0237830d1b6
Calvin Pangilinan <calvinpangilinan13@gmail.com>
2025-06-13 12:42:10 +0800

    resize

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -108,7 +108,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark navbar-modern">
     <div class="container">
         <a class="navbar-brand" href="<?php echo site_url('Welcome'); ?>">
-            <img src="<?php echo base_url('assets/images/lakbay.png.png'); ?>" alt="Logo">
+            <img src="<?php echo base_url('assets/images/lakbay.png.png'); ?>" alt="Logo" style="height: 60px;">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
